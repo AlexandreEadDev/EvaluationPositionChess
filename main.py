dea50cb0615c338158fe6944004e939cc99023fe
Alexandre <alexandre.gerard.dev@gmail.com>
2024-05-29 16:22:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,38 +4,30 @@ import chess
 import chess.engine
 import random
 import os
-import logging
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})  # Enable CORS for the specific frontend origin
 
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
-    logging.debug(f"Request method: {request.method}")
-    logging.debug(f"Request data: {request.json}")
+    fen = request.json['fen']
+    level = request.json['level']
+    board = chess.Board(fen)
 
-    if request.method == 'POST':
-        fen = request.json['fen']
-        level = request.json['level']
-        board = chess.Board(fen)
+    with chess.engine.SimpleEngine.popen_uci("stockfish-windows-x86-64-avx2.exe") as engine:
+        result = engine.analyse(board, chess.engine.Limit(time=0.1), multipv=5)
+        moves_scores = [(entry['pv'][0].uci(), entry['score'].white().score(mate_score=10000)) for entry in result]
 
-        with chess.engine.SimpleEngine.popen_uci("stockfish-windows-x86-64-avx2.exe") as engine:
-            result = engine.analyse(board, chess.engine.Limit(time=0.1), multipv=5)
-            moves_scores = [(entry['pv'][0].uci(), entry['score'].white().score(mate_score=10000)) for entry in result]
+        if level == 1:
+            chosen_move = random.choice(moves_scores[:5])[0]  # Chooses randomly from the top 5 moves
+        elif level == 2:
+            chosen_move = random.choice(moves_scores[:3])[0]  # Chooses randomly from the top 3 moves
+        else:  # level == 3 or any other value
+            chosen_move = moves_scores[0][0]  # Always chooses the best move
 
-            if level == 1:
-                chosen_move = random.choice(moves_scores[:5])[0]  # Chooses randomly from the top 5 moves
-            elif level == 2:
-                chosen_move = random.choice(moves_scores[:3])[0]  # Chooses randomly from the top 3 moves
-            else:  # level == 3 or any other value
-                chosen_move = moves_scores[0][0]  # Always chooses the best move
+        evaluation = moves_scores[0][1]  # Evaluation of the best move
 
-            evaluation = moves_scores[0][1]  # Evaluation of the best move
-
-        return jsonify({'evaluation': evaluation, 'best_move': chosen_move})
+    return jsonify({'evaluation': evaluation, 'best_move': chosen_move})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
